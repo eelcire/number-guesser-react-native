92e4dd5a85a2d1063d5f50d2a6bb748f7ddf786c
eelcire <eelcire.eelcire@hotmail.com>
2020-02-05 11:58:41 -0600

    start game screen

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import StartGameScreen from './screens/StartGameScreen'
+import Header from './components/Header'
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Header title = "Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    flex: 1
   },
 });
